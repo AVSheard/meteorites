b6ce5b494ff1f65b3360e529cc0e5514f1fb1036
AVSheard <a_sheard@yahoo.co.uk>
2020-04-06 17:08:33 +0100

    got the metorites to apear in a list displaying their longlitude and latitude

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,6 +5,17 @@ import { observer } from "mobx-react";
 import axios from "axios";
 import { meteoriteData } from "../stores/meteoriteData";
 
+// fall: "Fell"
+// geolocation: Object { latitude: "50.775", longitude: "6.08333" }
+// id: "1"
+// mass: "21"
+// name: "Aachen"
+// nametype: "Valid"
+// recclass: "L5"
+// reclat: "50.775000"
+// reclong: "6.083330"
+// year: "1880-01-01T00:00:00.000"
+
 export default observer(
 	class List extends Component {
 		componentDidMount() {
@@ -14,7 +25,20 @@ export default observer(
 		}
 
 		render() {
-			return <ul></ul>;
+			return (
+				<ul>
+					{meteoriteData.meteorites.length > 0 ? (
+						meteoriteData.meteorites.map((meteorite) => (
+							<li key={meteorite.id}>
+								{meteorite.name}: latitude {meteorite.reclat} longlitude{" "}
+								{meteorite.reclong}
+							</li>
+						))
+					) : (
+						<>loading...</>
+					)}
+				</ul>
+			);
 		}
 	}
 );
