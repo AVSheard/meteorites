67c1a3268547dc2c4f7cf5ceb1fd9f713346e7ed
AVSheard <a_sheard@yahoo.co.uk>
2020-04-06 15:12:22 +0100

    deleted unneed files created by starting react

---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+
+export default observer(
+	class List extends Component {
+		render() {
+			return <div></div>;
+		}
+	}
+);
